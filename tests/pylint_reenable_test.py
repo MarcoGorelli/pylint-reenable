cc462e7a1180fd817f74efc40c437ce714413fa0
MarcoGorelli <>
2022-12-01 16:22:01 +0000

    fix tests

--- tests/pylint_reenable_test.py
+++ tests/pylint_reenable_test.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pytest
 
-import pylint-reenable
+import pylint_reenable
 
 
 @pytest.fixture
@@ -12,15 +12,16 @@ def assert_rewrite(tmpdir):
         expected = s if expected is None else expected
         f = tmpdir.join('f.py')
         f.write(s)
-        assert pylint-reenable.fix_file(str(f)) == expected_retc
+        assert pylint_reenable.fix_file(str(f)) == expected_retc
         assert f.read() == expected
+
     return _assert
 
 
 def test_non_utf8_bytes(tmpdir, capsys):
     f = tmpdir.join('f.py')
     f.write_binary('x = "€"'.encode('cp1252'))
-    assert pylint-reenable.fix_file(str(f)) == 1
+    assert pylint_reenable.fix_file(str(f)) == 1
     out, _ = capsys.readouterr()
     assert out == f'{f} is non-utf8 (not supported)\n'
 
@@ -31,7 +32,7 @@ def test_non_utf8_bytes(tmpdir, capsys):
         '',  # noop
         '# hello\n',  # comment at beginning of file
         # still needed
-        'import os  # noqa\n',
+        'import os  # pylint: disable=unused-import\n',
         'import os  # NOQA\n',
         'import os  # noqa: F401\n',
         'import os  # noqa:F401\n',
@@ -42,13 +43,13 @@ def test_non_utf8_bytes(tmpdir, capsys):
         'from foo\\\nimport bar  # noqa\n',
         # don't rewrite syntax errors
         'import x  # noqa\nx() = 5\n',
-
         'A' * 65 + ' = int\n\n\n'
         'def f():\n'
         '    # type: () -> ' + 'A' * 65 + '  # noqa\n'
         '    pass\n',
         'def foo(w: Sequence[int], x: Sequence[int], y: int, z: int) -> bar: ...  # noqa: E501, F821\n',
         'def foo(w: Sequence[int]) -> bar:  # foobarfoobarfoobarfoobarfoobarfoo   # noqa: E501, F821\n',
+        '1 = 3  # pylint: disable=all\n',
     ),
 )
 def test_ok(assert_rewrite, src):
@@ -59,48 +60,64 @@ def test_ok(assert_rewrite, src):
     ('src', 'expected'),
     (
         # line comments
-        ('x = 1  # noqa\n', 'x = 1\n'),
-        ('import os  # noqa: F401,X999\n', 'import os  # noqa: F401\n'),
-        ('import os  # noqa:F401,X999\n', 'import os  # noqa: F401\n'),
-        ('# foo # noqa\nx = 1\n', '# foo\nx = 1\n'),
-        ('# noqa # foo\nx = 1\n', '# foo\nx = 1\n'),
+        ('x = 1  # pylint:disable=redefined-outer-name\n', 'x = 1\n'),
+        (
+            'import os  # pylint: disable=unused-import,foobar\n',
+            'import os  # pylint: disable=unused-import\n',
+        ),
+        (
+            'import os  # pylint:disable=unused-import,foobar\n',
+            'import os  # pylint: disable=unused-import\n',
+        ),
+        (
+            '# foo # pylint:disable-next=redefined-outer-name\nx = 1\n',
+            '# foo\nx = 1\n',
+        ),
+        (
+            '# pylint:disable=redefined-outer-name # foo\nx = 1\n',
+            '# foo\nx = 1\n',
+        ),
         (
             'try:\n'
             '    pass\n'
-            'except OSError:  # noqa hi\n'
+            'except OSError:  # pylint:disable=redefined-outer-name hi\n'
             '    pass\n',
-            'try:\n'
-            '    pass\n'
-            'except OSError:  # hi\n'
-            '    pass\n',
+            'try:\n' '    pass\n' 'except OSError:\n' '    pass\n',
         ),
         (
-            'import os  # noqa\n'
+            'import os  # pylint:disable=unused-import\n'
             '# hello world\n'
             'os\n',
-            'import os\n'
-            '# hello world\n'
-            'os\n',
+            'import os\n' '# hello world\n' 'os\n',
         ),
         (
-            '# a  # noqa: E501\n',
+            '# a  # pylint: disable=redefined-outer-name\n',
             '# a\n',
         ),
         pytest.param(
-            'if x==1:  # noqa: F401\n'
-            '    pass\n',
-            'if x==1:\n'
-            '    pass\n',
+            'if x==1:  # pylint:disable=foo\n' '    pass\n',
+            'if x==1:\n' '    pass\n',
             id='wrong noqa',
         ),
         pytest.param(
-            'x = 1  # foo # noqa: ABC123\n',
+            'x = 1  # foo # pylint: disable=ABC\n',
             'x = 1  # foo\n',
             id='multi-character noqa code',
         ),
+        (
+            'if True:\n' '    # pylint: disable-next=all\n' '    pass\n',
+            'if True:\n' '\n' '    pass\n',
+        ),
+        (
+            'if True:  '
+            '# pylint: disable-next=invalid-name,redefined-outer-name\n'
+            '    x=1\n',
+            'if True:  # pylint: disable-next=invalid-name\n' '    x=1\n',
+        ),
+        ('# pylint: disable-next=all\nx = 1\n', '\nx = 1\n'),
         # file comments
-        ('# flake8: noqa\nx = 1\n', 'x = 1\n'),
-        ('x = 1  # flake8: noqa\n', 'x = 1\n'),
+        ('# pylint: disable=all\nx = 1\n', '\nx = 1\n'),
+        ('x = 1  # pylint: disable=red\n', 'x = 1\n'),
     ),
 )
 def test_rewrite(assert_rewrite, src, expected):
@@ -110,8 +127,8 @@ def test_rewrite(assert_rewrite, src, expected):
 def test_main(tmpdir, capsys):
     f = tmpdir.join('f.py').ensure()
     g = tmpdir.join('g.py')
-    g.write('x = 1  # noqa\n')
-    ret = pylint-reenable.main((str(f), str(g)))
+    g.write('x = 1  # pylint: disable=foo\n')
+    ret = pylint_reenable.main((str(f), str(g)))
     assert ret == 1
     assert g.read() == 'x = 1\n'
     out, _ = capsys.readouterr()
@@ -123,6 +140,6 @@ def test_show_source_in_config(tmpdir, capsys):
     f.write('import os  # noqa\n')
     tmpdir.join('tox.ini').write('[flake8]\nshow_source = true\n')
     with tmpdir.as_cwd():
-        ret = pylint-reenable.main((str(f),))
+        ret = pylint_reenable.main((str(f),))
     assert ret == 0
     assert f.read() == 'import os  # noqa\n'
